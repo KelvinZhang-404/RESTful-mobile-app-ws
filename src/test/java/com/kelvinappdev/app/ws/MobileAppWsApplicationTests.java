f0dbe1873da03e92d753c87d836a3806c57f865b
Kelvin Zhang <lianxue1990@gmail.com>
2022-04-03 19:58:44 +1000

    initial commit

---
+++ src/test/java/com/kelvinappdev/app/ws/MobileAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvinappdev.app.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
