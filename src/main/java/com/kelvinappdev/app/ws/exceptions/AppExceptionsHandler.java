0fa5c8abc92ec6371aef7110c0d4b620221aa6f7
Kelvin Zhang <lianxue1990@gmail.com>
2022-04-03 21:01:49 +1000

    return custom error message object

---
+++ src/main/java/com/kelvinappdev/app/ws/exceptions/AppExceptionsHandler.java
@@ -0,0 +1,27 @@
+package com.kelvinappdev.app.ws.exceptions;
+
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.kelvinappdev.app.ws.ui.model.response.ErrorMessage;
+
+@ControllerAdvice
+public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
+	@ExceptionHandler(value = {Exception.class})
+	public ResponseEntity<Object> handleAnyExceptioin(Exception ex, WebRequest request) {
+		
+		String errorDesc = ex.getLocalizedMessage();
+		if(errorDesc == null) errorDesc = ex.toString();
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), errorDesc);
+		
+		return new ResponseEntity<>(
+				errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
